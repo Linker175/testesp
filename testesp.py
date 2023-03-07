1313c14f110ba0d8b1d66a295b77f13f2af2677a
Yoann <thebaultyoann@gmail.com>
2023-03-07 23:12:48 +0100

    kk

--- testesp.py
+++ testesp.py
@@ -2,15 +2,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
-"""@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET'])
 def get_request():
     # Gérer la requête GET ici
     return 'Hello ESP32!'
-"""
+
 
 @app.route('/')
 def index():
     return 'Hello World!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=443)
+    app.run(host='park.sandbox.ensea.fr', port=443)
