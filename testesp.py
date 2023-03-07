6ce535fe4c7de448e3910f38daa15183541791e6
Yoann <thebaultyoann@gmail.com>
2023-03-07 19:53:46 +0100

    maj hello world

--- testesp.py
+++ testesp.py
@@ -7,5 +7,9 @@ def get_request():
     # Gérer la requête GET ici
     return 'Hello ESP32!'
 
+@app.route('/')
+def index():
+    return 'Hello World!'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
