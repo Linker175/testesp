b0c405fa66b133ab512341d46b70fdd4a2c60555
Yoann <thebaultyoann@gmail.com>
2023-03-07 19:55:53 +0100

    maj hello world

--- testesp.py
+++ testesp.py
@@ -2,10 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+"""@app.route('/', methods=['GET'])
 def get_request():
     # Gérer la requête GET ici
     return 'Hello ESP32!'
+"""
 
 @app.route('/')
 def index():
